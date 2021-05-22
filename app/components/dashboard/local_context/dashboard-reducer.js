90ed8f92b9fff0a4367b61a432e6e9ecd044111c
endepointe <endepointe@gmail.com>
2021-05-22 11:21:11 -0700

    reviewing redux

--- app/components/dashboard/local_context/dashboard-reducer.js
+++ app/components/dashboard/local_context/dashboard-reducer.js
@@ -1,8 +1,43 @@
 // possible global state values would go here
+const initialState = { value: 0 };
 import CreateTask from '../CreateTask';
 import ViewTasks from '../ViewTasks';
 import ViewAllTaskPosts from '../ViewAllTaskPosts';
 import ViewAllContracts from '../ViewAllContracts';
+
+// Array reducer
+const arr = [1, 2, 3, 4, 5];
+const initialValue = 0;
+const total = arr.reduce(sum, initialValue);
+function sum(prevResult, currItem) {
+  return prevResult + currItem;
+}
+console.log("total: ", total)
+
+// Action object
+const addTodoAction = {
+  type: 'todos/todoAdded',
+  payload: 'take out the trash',
+}
+// Action Creators
+const addTodo = text => {
+  return {
+    type: 'todos/todoAdded',
+    payload: text,
+  }
+}
+
+function counterReducer(state = initialState, action) {
+  if (action.type === 'counter/increment') {
+    return {
+      ...state,
+      value: state.value + 1,
+    }
+  }
+  return state;
+}
+
+
 
 export const DashboardReducer = (props) => {
   const action = props.action;
